7b550a7e1c36be0de1d41325bbe3e6f16f702a54
Alex Dou <anhtudo97@gmail.com>
2025-04-27 03:10:05 +0700

    feat: footer

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,12 +1,12 @@
 import emojiStar from "@/assets/images/emojistar.png";
-import helixImage from "@/assets/images/helix.png";
+import helixImage from "@/assets/images/helix2.png";
 import Image from "next/image";
 
 export const CallToAction = () => {
   return <div className="bg-black text-white py-[72px] sm:py-24">
     <div className="container max-w-xl relative">
-      <Image src={helixImage} alt="emoji star" className="absolute top-6 left-[calc(100%+36px)]" />
-      <Image src={emojiStar} alt="emoji star" className="absolute -top-[-120px] right-[calc(100%+24px)]" />
+      <Image src={helixImage} alt="helix star" className="absolute top-6 left-[calc(100%+36px)] hidden sm:inline" />
+      <Image src={emojiStar} alt="emoji star" className="absolute -top-[-120px] right-[calc(100%+24px)] hidden sm:inline" />
       <h2 className="font-bold text-5xl sm:text-6xl tracking-tighter">Get instant access</h2>
       <p className="text-xl text-white/70 tracking-tighter">
         Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.
@@ -15,7 +15,7 @@ export const CallToAction = () => {
         <input
           type="email"
           placeholder="your@email.com"
-          className="h-12 bg-white/20 rounded-lg px-5 flex-1 font-medium placeholder:text-[#9CA3AF]" />
+          className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF] sm:flex-1" />
         <button className="bg-white text-black h-12 px-5 rounded-lg">
           Get access
         </button>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,22 @@
+import Instagram from "@/assets/icons/instagram.svg";
+import Tiktok from "@/assets/icons/tiktok.svg";
+import Twitter from "@/assets/icons/twitter.svg";
+import Linkedin from "@/assets/icons/linkedin.svg";
+import XSocial from "@/assets/icons/x-social.svg";
+
 export const Footer = () => {
-  return null;
+  return <footer className="py-5 bg-black text-white/60 border-t border-white/20">
+    <div className="container">
+      <div className="flex flex-col sm:flex-row sm:justify-between gap-4">
+        <div className="text-center">© 2024 Your Company, Inc. All rights reserved</div>
+        <ul className="flex justify-center gap-2.5">
+          <li><Instagram /></li>
+          <li><Tiktok /></li>
+          <li><Twitter /></li>
+          <li><Linkedin /></li>
+          <li><XSocial /></li>
+        </ul>
+      </div>
+    </div>
+  </footer>;
 };
